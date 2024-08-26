2c999ba3a1f69f71eee8ce0e19daae6b74c421c7
GHK <hongkai.gao@united-imaging.com>
2024-08-26 18:01:03 +0800

    做不出按钮说是

--- platform-frontend/src/App.js
+++ platform-frontend/src/App.js
@@ -2,37 +2,49 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+import { Button } from 'reactstrap';
+import QaList from "./QaList";
+//
+// class App extends Component {
+//
+//     state = {
+//         qas: []
+//     };
+//
+//     async componentDidMount() {
+//         const response = await fetch('/qa');
+//         const body = await response.json();
+//         this.setState({qas: body});
+//     }
+//
+//     render() {
+//         const {qas} = this.state;
+//         return (
+//             <div className="App">
+//                 <header className="App-header">
+//                     <img src={logo} className="App-logo" alt="logo"/>
+//                     <h3 className="App-title">QA总览</h3>
+//                     {qas.map(qa =>
+//                         <div key = {qa.qaid}>
+//                             {qa.questioncontent} ({qa.askerid})
+//                         </div>
+//                     )}
+//                 </header>
+//                 <p className="App-intro">
+//                     To get started, edit <code>src/App.js</code> and save to reload.
+//                 </p>
+//             </div>
+//
+//         );
+//     }
+// }
 
-    state = {
-        qas: []
-    };
-
-    async componentDidMount() {
-        const response = await fetch('/qa');
-        const body = await response.json();
-        this.setState({qas: body});
-    }
-
-    render() {
-        const {qas} = this.state;
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h3 className="App-title">QA总览</h3>
-                    {qas.map(qa =>
-                        <div key = {qa.qaid}>
-                            {qa.questioncontent} ({qa.askerid})
-                        </div>
-                    )}
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
-        );
-    }
+function App() {
+    return (
+        <div>
+            <QaList />
+        </div>
+    )
 }
 
 export default App;
