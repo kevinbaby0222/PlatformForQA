07872a09514cd954d9201a86ad5ad8fff569903a
GHK <hongkai.gao@united-imaging.com>
2024-08-26 16:00:26 +0800

    最基本的数据库元素展示

--- platform-frontend/src/App.js
+++ platform-frontend/src/App.js
@@ -1,25 +1,38 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        qas: []
+    };
+
+    async componentDidMount() {
+        const response = await fetch('/qa');
+        const body = await response.json();
+        this.setState({qas: body});
+    }
+
+    render() {
+        const {qas} = this.state;
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h3 className="App-title">QA总览</h3>
+                    {qas.map(qa =>
+                        <div key = {qa.qaid}>
+                            {qa.questioncontent} ({qa.askerid})
+                        </div>
+                    )}
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
