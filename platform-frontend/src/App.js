56d2d391e2598d6b81a7f1646328c6d214282101
GHK <hongkai.gao@united-imaging.com>
2024-08-27 18:04:28 +0800

    react学习

--- platform-frontend/src/App.js
+++ platform-frontend/src/App.js
@@ -1,17 +1,7 @@
-import {useState} from 'react';
-function App() {
-
-
-  const [content, setContent] = useState('标签默认内容')
-  function handleClick (e) {
-    setContent('新内容') 
-  }
+export default function App () {
   return (
     <>
-        <div>{content}</div>
-        <button onClick = {handleClick}>点击</button>
-    </>
-  );
-}
 
-export default App;
+    </>
+  )
+}
